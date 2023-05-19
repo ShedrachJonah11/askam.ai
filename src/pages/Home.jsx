c3ff93c7ad9e9a9d4b22de3182ae039497a80271
m@nji <spontaneousmanji@gmail.com>
2023-05-19 22:12:13 +0100

    chore: fix home page styles (#1)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,156 +14,154 @@ import Navbar from "../components/Navbar";
 const Home = () => {
   const { images } = img;
   return (
-    <>
+    <div className="home">
       <Navbar />
-      <div className="home">
-        <div className="container">
-          <section className="homeSectionFirst">
-            <div className="homeSectionIntro">
-              <button className="btn ejoy">Enjoy with Askami AI</button>
-              <h1>Experience cutting-edge AI at your fingertips.</h1>
-              <h2>
-                Obtain swift responses, engage in delightful discussions, request
-                scrumptious recipes, check weather forecasts, and so much more.
-                Effortlessly perform internet searches and rely on AI-powered
-                solutions to provide the information you need.
-              </h2>
-              <div className="homeJoinUs">
-                <div className="homeJoinUsImgs">
-                  <img src={profile} alt="profile1" />
-                  <img src={profile} alt="profile2" className="homeJoinHide" />
-                  <img src={profile} alt="profile3" className="homeJoinHide" />
-                  <img src={profile} alt="profile4" className="homeJoinHide" />
-                  <div className="homejoinText">30+</div>
-                </div>
-                <p className="homeJoinUsText">
-                  Join us <a href="mailto:@akami.ai">@akami.ai</a>
-                </p>
+      <div className="TopContainer">
+        <section className="homeSectionFirst">
+          <div className="homeSectionIntro">
+            <button className="btn ejoy">Enjoy with Askami AI</button>
+            <h1>Experience cutting-edge AI at your fingertips.</h1>
+            <h2>
+              Obtain swift responses, engage in delightful discussions, request
+              scrumptious recipes, check weather forecasts, and so much more.
+              Effortlessly perform internet searches and rely on AI-powered
+              solutions to provide the information you need.
+            </h2>
+            <div className="homeJoinUs">
+              <div className="homeJoinUsImgs">
+                <img src={profile} alt="profile1" />
+                <img src={profile} alt="profile2" className="homeJoinHide" />
+                <img src={profile} alt="profile3" className="homeJoinHide" />
+                <img src={profile} alt="profile4" className="homeJoinHide" />
+                <div className="homejoinText">30+</div>
               </div>
-              <div className="homeCodeMapping">
-                <img src={arrow2} alt="arrow2" className="arrow2Img" />
-                <img src={codeblock} alt="codeblock" className="codeblock" />
-                <img src={arrow1} alt="arrow1" className="arrow1Img" />
-              </div>
-            </div>
-            <div className="homeSectionChatBot">
-              <img src={chatbot} alt="chatbot" className="chatbotImg" />
-            </div>
-          </section>
-          <section className="homeSectionFeatures">
-            <header className="homeSectionFeaturesHeader">
-              <h1>Our Features</h1>
-            </header>
-            <article className="homeSectionFeaturesText">
-              <p>
-                Say hello to our latest integration - the AI-powered WhatsApp!
-                With advanced natural language processing and machine learning
-                algorithms, it's now easier than ever to create and summarize
-                content right within the app. From generating custom messages to
-                summarizing lengthy documents, this cutting-edge technology does
-                it all.
+              <p className="homeJoinUsText">
+                Join us <a href="mailto:@akami.ai">@akami.ai</a>
               </p>
-            </article>
-          </section>
-          <section className="homeSectionFocus">
-            <header className="homeSectionFocusHeader">
-              <h1>
-                Our focus is on developing secure, high-performing language models
-                that cater to the needs of enterprises.
-              </h1>
-            </header>
-            <article className="homeSectionFocusText">
-              <p>
-                To ensure exceptional support and data security, our
-                high-performance language models can be tailored to fit public,
-                private, or hybrid clouds. We offer customizable options for our
-                language models, allowing you to choose the best fit for your
-                specific needs.
-              </p>
-            </article>
-          </section>
-          <section className="homeSectionComponent">
-            {images.map((image) => {
-              return <ImageComp key={image.id} img={image.src} alt={image.alt} />;
-            })}
-          </section>
-        </div>
-        <section className="homeSectionGenerate">
-          <article className="homeSectionGenerateTop">
-            <div className="sectionGenerateIntro">
-              <h1>
-                Global companies can leverage our platform and API, which are
-                designed to be user-friendly, to cater to various use cases.
-              </h1>
             </div>
-            <div className="homeSectionGenerateTopCard">
-              <h1>GENERATE</h1>
-              <div className="GenerateCard"></div>
-            </div>
-          </article>
-          <article className="homeSectionGenerateBottom">
-            <div className="firstCard">
-              <h1>GENERATE</h1>
-              <div className="GenerateCard">
-                <img src={Ai} alt="aibot" />
-              </div>
-            </div>
-            <div className="secondCard">
-              <h1>GENERATE</h1>
-              <div className="GenerateCard"></div>
-            </div>
-          </article>
-        </section>
-        <footer className="homeFooter">
-          <div className="container">
-            <div className="footerTable">
-              <ul>
-                <li className="footerliHeader">Product</li>
-                <li>Summarize</li>
-                <li>Classify</li>
-                <li>Rerank</li>
-                <li>Summarize</li>
-              </ul>
-              <ul>
-                <li className="footerliHeader">About</li>
-                <li>Summarize</li>
-                <li>Classify</li>
-                <li>Rerank</li>
-                <li>Summarize</li>
-              </ul>
-              <ul>
-                <li className="footerliHeader">Developers</li>
-                <li>Summarize</li>
-                <li>Classify</li>
-                <li>Rerank</li>
-                <li>Summarize</li>
-              </ul>
-              <ul>
-                <li className="footerliHeader">Trusted</li>
-                <li>Summarize</li>
-                <li>Classify</li>
-                <li>Rerank</li>
-                <li>Summarize</li>
-              </ul>
-            </div>
-            <h1>ASAKMI</h1>
-            <div className="footerInfo">
-              <ul className="footerSocials">
-                <li>Twitter</li>
-                <li>Linkedin</li>
-                <li>Linkedin</li>
-                <li>Email</li>
-              </ul>
-              <ul className="footerTerms">
-                <li>Privacy</li>
-                <li>Term of Use</li>
-                <li>@askami 2023</li>
-              </ul>
+            <div className="homeCodeMapping">
+              <img src={arrow2} alt="arrow2" className="arrow2Img" />
+              <img src={codeblock} alt="codeblock" className="codeblock" />
+              <img src={arrow1} alt="arrow1" className="arrow1Img" />
             </div>
           </div>
-        </footer>
+          <div className="homeSectionChatBot">
+            <img src={chatbot} alt="chatbot" className="chatbotImg" />
+          </div>
+        </section>
+        <section className="homeSectionFeatures">
+          <header className="homeSectionFeaturesHeader">
+            <h1>Our Features</h1>
+          </header>
+          <article className="homeSectionFeaturesText">
+            <p>
+              Say hello to our latest integration - the AI-powered WhatsApp!
+              With advanced natural language processing and machine learning
+              algorithms, it's now easier than ever to create and summarize
+              content right within the app. From generating custom messages to
+              summarizing lengthy documents, this cutting-edge technology does
+              it all.
+            </p>
+          </article>
+        </section>
+        <section className="homeSectionFocus">
+          <header className="homeSectionFocusHeader">
+            <h1>
+              Our focus is on developing secure, high-performing language models
+              that cater to the needs of enterprises.
+            </h1>
+          </header>
+          <article className="homeSectionFocusText">
+            <p>
+              To ensure exceptional support and data security, our
+              high-performance language models can be tailored to fit public,
+              private, or hybrid clouds. We offer customizable options for our
+              language models, allowing you to choose the best fit for your
+              specific needs.
+            </p>
+          </article>
+        </section>
+        <section className="homeSectionComponent">
+          {images.map((image) => {
+            return <ImageComp key={image.id} img={image.src} alt={image.alt} />;
+          })}
+        </section>
       </div>
-    </>
+      <section className="homeSectionGenerate">
+        <article className="homeSectionGenerateTop">
+          <div className="sectionGenerateIntro">
+            <h1>
+              Global companies can leverage our platform and API, which are
+              designed to be user-friendly, to cater to various use cases.
+            </h1>
+          </div>
+          <div className="homeSectionGenerateTopCard">
+            <h1>GENERATE</h1>
+            <div className="GenerateCard"></div>
+          </div>
+        </article>
+        <article className="homeSectionGenerateBottom">
+          <div className="firstCard">
+            <h1>GENERATE</h1>
+            <div className="GenerateCard">
+              <img src={Ai} alt="aibot" />
+            </div>
+          </div>
+          <div className="secondCard">
+            <h1>GENERATE</h1>
+            <div className="GenerateCard"></div>
+          </div>
+        </article>
+      </section>
+      <footer className="homeFooter">
+        <div className="footer-container">
+          <div className="footerTable">
+            <ul>
+              <li className="footerliHeader">Product</li>
+              <li>Summarize</li>
+              <li>Classify</li>
+              <li>Rerank</li>
+              <li>Summarize</li>
+            </ul>
+            <ul>
+              <li className="footerliHeader">About</li>
+              <li>Summarize</li>
+              <li>Classify</li>
+              <li>Rerank</li>
+              <li>Summarize</li>
+            </ul>
+            <ul>
+              <li className="footerliHeader">Developers</li>
+              <li>Summarize</li>
+              <li>Classify</li>
+              <li>Rerank</li>
+              <li>Summarize</li>
+            </ul>
+            <ul>
+              <li className="footerliHeader">Trusted</li>
+              <li>Summarize</li>
+              <li>Classify</li>
+              <li>Rerank</li>
+              <li>Summarize</li>
+            </ul>
+          </div>
+          <h1>ASAKMI</h1>
+          <div className="footerInfo">
+            <ul className="footerSocials">
+              <li>Twitter</li>
+              <li>Linkedin</li>
+              <li>Linkedin</li>
+              <li>Email</li>
+            </ul>
+            <ul className="footerTerms">
+              <li>Privacy</li>
+              <li>Term of Use</li>
+              <li>@askami 2023</li>
+            </ul>
+          </div>
+        </div>
+      </footer>
+    </div>
   );
 };
 
