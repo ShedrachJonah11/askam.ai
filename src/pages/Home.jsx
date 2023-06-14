e6ef0b0594bcd91c920faa3de8a0a78e176c28d5
ShedrachJonah11 <shedrachjonah11@gmail.com>
2023-06-14 21:15:34 +0100

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 import "./styles/Home.css";
-import ChatBot from "../components/ChatBot";
 import chatbot from "./../assets/chatbot.png";
 import codeblock from "./../assets/codeblock.png";
 import arrow1 from "./../assets/arrow1.png";
 import arrow2 from "./../assets/arrow2.png";
 import profile from "./../assets/profile.png";
-import ImageComp from "../components/ImageComp";
 import img from "../images.json";
-import Ai from "./../assets/Aichat.png";
 import Navbar from "../components/Navbar";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   const { images } = img;
@@ -45,73 +43,29 @@ const Home = () => {
             </div>
           </div>
           <div className="homeSectionChatBot">
-            <img src={chatbot} alt="chatbot" className="chatbotImg" />
+            <img src={chatbot} alt="chatbot" className="chatbotImg1" />
+            <img src={chatbot} alt="chatbot" className="chatbotImg2" />
           </div>
         </section>
-        <section className="homeSectionFeatures">
-          <header className="homeSectionFeaturesHeader">
-            <h1>Our Features</h1>
-          </header>
-          <article className="homeSectionFeaturesText">
+
+        <section className="homeSectionTransform">
+          <div>
+            <h1>Transform your products with Askam.ai</h1>
             <p>
-              Say Hello to our latest integration - the AI-powered WhatsApp!
-              With advanced natural language processing and machine learning
-              algorithms, it's now easier than ever to create and summarize
-              content right within the app. From generating custom messages to
-              summarizing lengthy documents, this cutting-edge technology does
-              it all.
+              Easy as ABC, Send hello to your newest buddy, Save the number{" "}
+              <Link to="https://wa.me/+1415-523-8886">+1 (415) 523-8886</Link>{" "}
+              on your Whatsapp to get started Use the code{" "}
+              <span>"Join bugun-large"</span> to askam anything!!
             </p>
-          </article>
-        </section>
-        <section className="homeSectionFocus">
-          <header className="homeSectionFocusHeader">
-            <h1>Get Started</h1>
-            <h2>Easy as ABC, Send hello to your newest buddy,</h2>
-          </header>
-          <article className="homeSectionFocusText">
-            On your Whatsapp to get Started
-          </article>
-          <h1 className="homeSectionFocusCode">
-            Use the code{" "}
-            <span className="homeSectionFocusSpan">"Join begun-large"</span> to
-            askam anything
-          </h1>
-          <button className="homeSectionFocusButton">
-            <a href="http://wa.me/+14155238886">Get Started</a>
-          </button>
-        </section>
-        <section className="homeSectionComponent">
-          {images.map((image) => {
-            return <ImageComp key={image.id} img={image.src} alt={image.alt} />;
-          })}
-        </section>
-      </div>
-      <section className="homeSectionGenerate">
-        <article className="homeSectionGenerateTop">
-          <div className="sectionGenerateIntro">
-            <h1>
-              Global companies can leverage our platform and API, which are
-              designed to be user-friendly, to cater to various use cases.
-            </h1>
           </div>
-          <div className="homeSectionGenerateTopCard">
-            <h1>GENERATE</h1>
-            <div className="GenerateCard"></div>
-          </div>
-        </article>
-        <article className="homeSectionGenerateBottom">
-          <div className="firstCard">
-            <h1>GENERATE</h1>
-            <div className="GenerateCard">
-              <img src={Ai} alt="aibot" />
+          <div className="homeSectionTransformContainer">
+            <h4>Passcode</h4>
+            <div className="homeSectionTransformContain">
+              <h1></h1>
             </div>
           </div>
-          <div className="secondCard">
-            <h1>GENERATE</h1>
-            <div className="GenerateCard"></div>
-          </div>
-        </article>
-      </section>
+        </section>
+      </div>
       <footer className="homeFooter">
         <div className="footer-container">
           <div className="footerTable">
